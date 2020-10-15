ca898e146a49d6f4f3dec4ff78bd7090b4667009
Andreas Fischer <afischer95@gmx.net>
2020-10-15 15:01:17 +0200

    created tests for GHappy and removed bug

--- src/main/java/tudelft/ghappy/GHappy.java
+++ src/main/java/tudelft/ghappy/GHappy.java
@@ -4,15 +4,23 @@ public class GHappy {
 
     public boolean gHappy(String str) {
         assert str!=null;
+        int counter=0;
         for(int i = 0; i < str.length(); i++) {
             if(str.charAt(i) == 'g') {
-                if (i >= 0 && str.charAt(i-1) == 'g') { continue; }
-                if (i+1 < str.length() && str.charAt(i+1) == 'g') { continue; }
+                if (i > 0 && str.charAt(i-1) == 'g') {
+                    counter++;
+                    continue;
+                }
+                if (i+1 < str.length() && str.charAt(i+1) == 'g') {
+                    counter++;
+                    continue;
+                }
                 return false;
             }
         }
-
-        return true;
-
+        if(counter >= 1) {
+            return true;
+        }
+        return false;
     }
 }
--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,24 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 public class GHappyTest {
+
+    private GHappy happy;
+    
+    @BeforeEach
+    public void initialize() {
+        this.happy = new GHappy();
+    }
+    
+    @ParameterizedTest(name = "string={0}, result={1}")
+    @CsvSource({"ff,false", "g,false", "gg,true", "ggg,true", "ggxg,false", "ggxgg,true", "gxg,false", "x,false", "xg,false"})
+    public void totalIsTooBig(String string, boolean expectedResult) {
+        boolean result = happy.gHappy(string);
+        Assertions.assertEquals(expectedResult, result);
+    }
+
 }
